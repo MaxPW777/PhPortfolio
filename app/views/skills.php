9f3b8e9e88abb0d07388d157d299e7341cb9dca4
Maximilian <mpinderwhite@gmail.com>
2023-12-19 17:41:28 +0100

    [+] implemented images

--- app/views/skills.php
+++ app/views/skills.php
@@ -7,7 +7,7 @@
 
     foreach ($skills as $skill) {
         echo '<div class="skill">';
-        echo '<img src="assets/github-mark-white.svg" alt="">'; // Adjust the image source as needed
+        echo '<img src="assets/' . htmlspecialchars($skill['image']) . '" alt="' . htmlspecialchars($skill['image']) . '">'; // Adjust the image source as needed
         echo '<h3>' . htmlspecialchars($skill['SkillName']) . '</h3>'; // Use htmlspecialchars for security
         echo '</div>';
     }
